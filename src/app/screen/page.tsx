918d9ec2dc2d30fbf195b23593f867c89b758fb7
ezekielgbustamante@iskolarngbayan.pup.edu.ph <ezekielgbustamante@iskolarngbayan.pup.edu.ph>
2025-11-22 16:04:55 +0800

    Add Screen component with background and contents structure

---
+++ src/app/screen/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import ScreenBg from "@/components/backgrounds/Screen";
+import ScreenContents from "@/components/sections/ScreenContents";
+
+export default function Screen() {
+    return (
+        <ScreenBg>
+            <ScreenContents />
+        </ScreenBg>
+    );
+}
